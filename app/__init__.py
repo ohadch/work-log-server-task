19dde6666d37f98faddb1e75979c2e067b06366c
Ohad <ohad@salaryo.com>
2021-10-28 10:06:21 +0300

    Implements start_work and end_work routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+import names
+
+from flask import Flask, request, jsonify
 
 from db import WorkLogDatabase
+from exceptions import UserIsAlreadyOccupiedException
 
 
 def create_app(test_config=None):
@@ -12,6 +15,36 @@ def create_app(test_config=None):
     @app.route('/')
     def hello():
         return jsonify({"message": "Hello world"}), 200
+
+    @app.route('/work/start', methods=['POST'])
+    def start_work():
+        if request.method == "POST":
+            data = request.json
+            user = data.get("user", names.get_full_name())
+            assignment = data.get("assignment",
+                                  f"Sample assignment {len(set([log.assignment for log in db.get_all()]))}")
+
+            try:
+                log = db.add_log(user, assignment)
+                return jsonify({"log": log.__dict__()}), 200
+            except UserIsAlreadyOccupiedException as e:
+                return jsonify({"error": f"{e}"}), 403
+            except Exception:
+                return jsonify({"error": "An unknown error occurred"}), 500
+
+    @app.route('/work/end', methods=['POST'])
+    def end_work():
+        if request.method == "POST":
+            data = request.json
+            user = data.get("user")
+            if user is None:
+                return jsonify({"error": f"Must provide user in order to end his work"}), 400
+
+            try:
+                log = db.end_log(user)
+                return jsonify({"log": log.__dict__()}), 200
+            except UserIsAlreadyOccupiedException as e:
+                return jsonify({"error": f"{e}"}), 403
 
     @app.route('/report')
     def get_report():
